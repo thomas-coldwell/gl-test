0d824e9bb732c36aeb3622f7d126cdabaf471d03
thomas-coldwell <tcoldwell72@gmail.com>
2020-10-06 10:37:07 +0100

    Tried making it use the imperative API

--- App.tsx
+++ App.tsx
@@ -16,8 +16,7 @@ import { Asset } from "expo-asset";
 import { Slider } from "@miblanchard/react-native-slider";
 //@ts-ignore
 import _ from "lodash";
-
-const testImageUrl = "https://picsum.photos/id/1036/1280/720";
+import { Buffer } from "buffer";
 
 const vertShader = `
 precision highp float;
@@ -50,7 +49,7 @@ void main () {
   vec4 color = texture2D(texture, vec2(uv.x, uv.y));
   color *= gauss(sigma, 0.0);
 
-  for (int i = -100; i <= 100; i++) {
+  for (int i = -50; i <= 50; i++) {
     if (i >= -radius && i <= radius) {
       float offset = float(i);
       // Caclulate the current pixel index
@@ -104,80 +103,84 @@ export default function App() {
   const glVerts = useRef<Float32Array>();
   const originalImage = useRef<any>();
 
-  const [blur, setBlur] = useState(1);
+  const [blur, setBlur] = useState(40);
 
   const performBlur = async () => {
-    // Get the GL context, program and verts from its initial setup
-    const gl = glCtx.current;
-    const program = glProgram.current;
-    const verts = glVerts.current;
-    const image = originalImage.current;
-    // Check all are not null
-    if (gl && program && verts && image) {
-      // Perform seperate vertical and horizonal blur passes for
-      // efficiency - http://rastergrid.com/blog/2010/09/efficient-gaussian-blur-with-linear-sampling/
-      // Set the blur radius for the gaussian blur
-      gl.uniform1i(gl.getUniformLocation(program, "texture"), 0);
-      gl.uniform1i(gl.getUniformLocation(program, "radius"), blur);
-      gl.uniform1i(gl.getUniformLocation(program, "pass"), 0);
-      // Setup so first pass renders to a texture rather than to canvas
-      // Create and bind the framebuffer
-      const firstPassTexture = gl.createTexture();
-      // Set the active texture to the texture 0 binding (0-30)
-      gl.activeTexture(gl.TEXTURE1);
-      // Bind the texture to WebGL stating what type of texture it is
-      gl.bindTexture(gl.TEXTURE_2D, firstPassTexture);
-      // Set some parameters for the texture
-      gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
-      gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
-      // Then set the data of this texture using texImage2D
-      gl.texImage2D(
-        gl.TEXTURE_2D,
-        0,
-        gl.RGBA,
-        image.width,
-        image.height,
-        0,
-        gl.RGBA,
-        gl.UNSIGNED_BYTE,
-        null
-      );
-      const fb = gl.createFramebuffer();
-      gl.bindFramebuffer(gl.FRAMEBUFFER, fb);
-      // attach the texture as the first color attachment
-      const attachmentPoint = gl.COLOR_ATTACHMENT0;
-      gl.framebufferTexture2D(
-        gl.FRAMEBUFFER,
-        attachmentPoint,
-        gl.TEXTURE_2D,
-        firstPassTexture,
-        0
-      );
-      // Actually draw using the shader program we setup!
-      gl.clearColor(0, 0, 1, 1);
-      gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-      gl.drawArrays(gl.TRIANGLES, 0, verts.length / 2);
-      // Fab now we want to do a second pass - let's use the first pass
-      // texture we just wrote to
-      gl.bindFramebuffer(gl.FRAMEBUFFER, null);
-      gl.uniform1i(gl.getUniformLocation(program, "texture"), 1);
-      gl.uniform1i(gl.getUniformLocation(program, "pass"), 1);
-      gl.clearColor(0, 0, 1, 1);
-      gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-      gl.drawArrays(gl.TRIANGLES, 0, verts.length / 2);
-      gl.endFrameEXP();
-    }
+    _.throttle(
+      async () => {
+        // Get the GL context, program and verts from its initial setup
+        const gl = glCtx.current;
+        const program = glProgram.current;
+        const verts = glVerts.current;
+        const image = originalImage.current;
+        // Check all are not null
+        if (gl && program && verts && image) {
+          // Perform seperate vertical and horizonal blur passes for
+          // efficiency - http://rastergrid.com/blog/2010/09/efficient-gaussian-blur-with-linear-sampling/
+          // Set the blur radius for the gaussian blur
+          gl.uniform1i(gl.getUniformLocation(program, "texture"), 0);
+          gl.uniform1i(gl.getUniformLocation(program, "radius"), blur);
+          gl.uniform1i(gl.getUniformLocation(program, "pass"), 0);
+          // Setup so first pass renders to a texture rather than to canvas
+          // Create and bind the framebuffer
+          const firstPassTexture = gl.createTexture();
+          // Set the active texture to the texture 0 binding (0-30)
+          gl.activeTexture(gl.TEXTURE1);
+          // Bind the texture to WebGL stating what type of texture it is
+          gl.bindTexture(gl.TEXTURE_2D, firstPassTexture);
+          // Set some parameters for the texture
+          gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+          gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+          // Then set the data of this texture using texImage2D
+          gl.texImage2D(
+            gl.TEXTURE_2D,
+            0,
+            gl.RGBA,
+            image.width,
+            image.height,
+            0,
+            gl.RGBA,
+            gl.UNSIGNED_BYTE,
+            null
+          );
+          const fb = gl.createFramebuffer();
+          gl.bindFramebuffer(gl.FRAMEBUFFER, fb);
+          // attach the texture as the first color attachment
+          const attachmentPoint = gl.COLOR_ATTACHMENT0;
+          gl.framebufferTexture2D(
+            gl.FRAMEBUFFER,
+            attachmentPoint,
+            gl.TEXTURE_2D,
+            firstPassTexture,
+            0
+          );
+          // Actually draw using the shader program we setup!
+          gl.drawArrays(gl.TRIANGLES, 0, verts.length / 2);
+          // Fab now we want to do a second pass - let's use the first pass
+          // texture we just wrote to
+          gl.bindFramebuffer(gl.FRAMEBUFFER, null);
+          gl.uniform1i(gl.getUniformLocation(program, "texture"), 1);
+          gl.uniform1i(gl.getUniformLocation(program, "pass"), 1);
+          gl.drawArrays(gl.TRIANGLES, 0, verts.length / 2);
+          gl.endFrameEXP();
+        }
+      },
+      50,
+      { leading: true }
+    )();
   };
 
   const takeSnapshot = async () => {
     const gl = glCtx.current;
-    if (gl) {
+    const verts = glVerts.current;
+    if (gl && verts) {
+      gl.drawArrays(gl.TRIANGLES, 0, verts.length / 2);
       const output = await GLView.takeSnapshotAsync(gl);
       if (Platform.OS === "web") {
-        var reader = new window.FileReader();
-        reader.readAsDataURL(output.uri as Blob);
-        reader.onloadend = function () {
-          var base64data = reader.result;
+        const fileReaderInstance = new FileReader();
+        fileReaderInstance.readAsDataURL(output.uri as any);
+        fileReaderInstance.onload = () => {
+          const base64data = fileReaderInstance.result;
           setImg({ uri: base64data });
         };
       } else {
@@ -187,118 +190,127 @@ export default function App() {
   };
 
   useEffect(() => {
-    if (!_.isEmpty(img)) performBlur();
+    if (!_.isEmpty(img)) {
+      performBlur();
+    }
   }, [blur]);
 
-  const onContextCreate = async (gl: ExpoWebGLRenderingContext) => {
-    console.log(gl.getParameter(gl.VERSION));
-    console.log(gl.getParameter(gl.SHADING_LANGUAGE_VERSION));
-    console.log(gl.getParameter(gl.VENDOR));
-    // Setup the shaders for our GL context so it draws from texImage2D
-    const vert = gl.createShader(gl.VERTEX_SHADER);
-    const frag = gl.createShader(gl.FRAGMENT_SHADER);
-    if (vert && frag) {
-      // Set the source of the shaders and compile them
-      gl.shaderSource(vert, vertShader);
-      gl.compileShader(vert);
-      gl.shaderSource(frag, fragShader);
-      gl.compileShader(frag);
-      // Create a WebGL program so we can link the shaders together
-      const program = gl.createProgram();
-      if (program) {
-        // Attach both the vertex and frag shader to the program
-        gl.attachShader(program, vert);
-        gl.attachShader(program, frag);
-        // Link the program - ensures that vetex and frag shaders are compatible
-        // with each other
-        gl.linkProgram(program);
-        // Tell GL we ant to now use this program
-        gl.useProgram(program);
-        // Create a buffer on the GPU and assign its type as array buffer
-        const buffer = gl.createBuffer();
-        gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
-        // Create the verticies for WebGL to form triangles on the screen
-        // using the vertex shader which forms a square or rectangle in this case
-        const verts = new Float32Array([
-          -1,
-          -1,
-          1,
-          -1,
-          1,
-          1,
-          -1,
-          -1,
-          -1,
-          1,
-          1,
-          1,
-        ]);
-        // Actually pass the verticies into the buffer and tell WebGL this is static
-        // for optimisations
-        gl.bufferData(gl.ARRAY_BUFFER, verts, gl.STATIC_DRAW);
-        // Get the index in memory for the position attribute defined in the
-        // vertex shader
-        const positionAttrib = gl.getAttribLocation(program, "position");
-        gl.enableVertexAttribArray(positionAttrib); // Enable it i guess
-        // Tell the vertex shader how to process this attribute buffer
-        gl.vertexAttribPointer(positionAttrib, 2, gl.FLOAT, false, 0, 0);
-        // Fetch an expo asset which can passed in as the source for the
-        // texImage2D
-        const asset = Asset.fromModule(require("./assets/test.jpg"));
-        await asset.downloadAsync();
-        if (asset.width && asset.height) {
-          // Create some space in memory for a texture
-          const texture = gl.createTexture();
-          // Set the active texture to the texture 0 binding (0-30)
-          gl.activeTexture(gl.TEXTURE0);
-          // Bind the texture to WebGL stating what type of texture it is
-          gl.bindTexture(gl.TEXTURE_2D, texture);
-          // Set some parameters for the texture
-          gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
-          gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
-          // Then set the data of this texture using texImage2D
-          gl.texImage2D(
-            gl.TEXTURE_2D,
-            0,
-            gl.RGBA,
-            gl.RGBA,
-            gl.UNSIGNED_BYTE,
-            asset as any
-          );
-          // Set a bunch of uniforms we want to pass into our fragement shader
-          gl.uniform1i(gl.getUniformLocation(program, "texture"), 0);
-          gl.uniform1f(gl.getUniformLocation(program, "width"), asset.width);
-          gl.uniform1f(gl.getUniformLocation(program, "height"), asset.height);
-          // Set some refs for things like the GL context, programs and verts we'll want
-          // to use at draw time
-          glCtx.current = gl;
-          glProgram.current = program;
-          glVerts.current = verts;
-          originalImage.current = asset;
-          await performBlur();
-          await takeSnapshot();
+  useEffect(() => {
+    const onContextCreate = async () => {
+      // Load in the asset and get its height and width
+      const asset = Asset.fromModule(require("./assets/test.jpg"));
+      await asset.downloadAsync();
+      if (asset.width && asset.height) {
+        // Imperatively creat a GL instance
+        const gl = await GLView.createContextAsync();
+        // gl.clear(gl.COLOR_BUFFER_BIT);
+        // gl.canvas.width = asset.width;
+        // gl.canvas.height = asset.height;
+        //gl.viewport(0, 0, asset.width, asset.height);
+        //console.log(gl.drawingBufferHeight, gl.drawingBufferWidth);
+        // Setup the shaders for our GL context so it draws from texImage2D
+        const vert = gl.createShader(gl.VERTEX_SHADER);
+        const frag = gl.createShader(gl.FRAGMENT_SHADER);
+        if (vert && frag) {
+          // Set the source of the shaders and compile them
+          gl.shaderSource(vert, vertShader);
+          gl.compileShader(vert);
+          gl.shaderSource(frag, fragShader);
+          gl.compileShader(frag);
+          // Create a WebGL program so we can link the shaders together
+          const program = gl.createProgram();
+          if (program) {
+            // Attach both the vertex and frag shader to the program
+            gl.attachShader(program, vert);
+            gl.attachShader(program, frag);
+            // Link the program - ensures that vetex and frag shaders are compatible
+            // with each other
+            gl.linkProgram(program);
+            // Tell GL we ant to now use this program
+            gl.useProgram(program);
+            // Create a buffer on the GPU and assign its type as array buffer
+            const buffer = gl.createBuffer();
+            gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+            // Create the verticies for WebGL to form triangles on the screen
+            // using the vertex shader which forms a square or rectangle in this case
+            const verts = new Float32Array([
+              -1,
+              -1,
+              1,
+              -1,
+              1,
+              1,
+              -1,
+              -1,
+              -1,
+              1,
+              1,
+              1,
+            ]);
+            // Actually pass the verticies into the buffer and tell WebGL this is static
+            // for optimisations
+            gl.bufferData(gl.ARRAY_BUFFER, verts, gl.STATIC_DRAW);
+            // Get the index in memory for the position attribute defined in the
+            // vertex shader
+            const positionAttrib = gl.getAttribLocation(program, "position");
+            gl.enableVertexAttribArray(positionAttrib); // Enable it i guess
+            // Tell the vertex shader how to process this attribute buffer
+            gl.vertexAttribPointer(positionAttrib, 2, gl.FLOAT, false, 0, 0);
+            // Fetch an expo asset which can passed in as the source for the
+            // texImage2D
+
+            // Create some space in memory for a texture
+            const texture = gl.createTexture();
+            // Set the active texture to the texture 0 binding (0-30)
+            gl.activeTexture(gl.TEXTURE0);
+            // Bind the texture to WebGL stating what type of texture it is
+            gl.bindTexture(gl.TEXTURE_2D, texture);
+            // Set some parameters for the texture
+            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+            // Then set the data of this texture using texImage2D
+            gl.texImage2D(
+              gl.TEXTURE_2D,
+              0,
+              gl.RGBA,
+              gl.RGBA,
+              gl.UNSIGNED_BYTE,
+              asset as any
+            );
+            // Set a bunch of uniforms we want to pass into our fragement shader
+            gl.uniform1i(gl.getUniformLocation(program, "texture"), 0);
+            gl.uniform1f(gl.getUniformLocation(program, "width"), asset.width);
+            gl.uniform1f(
+              gl.getUniformLocation(program, "height"),
+              asset.height
+            );
+            // Set some refs for things like the GL context, programs and verts we'll want
+            // to use at draw time
+            glCtx.current = gl;
+            glProgram.current = program;
+            glVerts.current = verts;
+            originalImage.current = asset;
+            await performBlur();
+            await takeSnapshot();
+          }
         }
       }
-    }
-  };
+    };
+    onContextCreate();
+  }, []);
 
   return (
     <View style={styles.container}>
-      <Image style={styles.image} source={{ uri: testImageUrl }} />
+      <Image style={styles.image} source={require("./assets/test.jpg")} />
       <Slider
         value={blur}
         onValueChange={(value) =>
           setBlur(Math.round((value as unknown) as number))
         }
         minimumValue={1}
-        maximumValue={100}
+        maximumValue={50}
         containerStyle={{ width: 300 }}
-        onSlidingComplete={() => {}}
-      />
-      <GLView
-        style={[styles.image, { transform: [{ scaleY: -1 }] }]}
-        pointerEvents="none"
-        onContextCreate={onContextCreate}
+        onSlidingComplete={() => takeSnapshot()}
       />
       {!_.isEmpty(img) && (
         <Image
@@ -318,8 +330,8 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
   image: {
-    height: 720 / PixelRatio.get(),
-    width: 1280 / PixelRatio.get(),
+    height: 300,
+    width: 500,
     margin: 5,
     resizeMode: "contain",
   },
